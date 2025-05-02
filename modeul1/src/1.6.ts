91fc6666ba314794aafe398a2b12ac2b745450d3
shakhawat2323 <shakhawathossain208@gmail.com>
2025-05-02 21:12:02 +0600

    function type

---
+++ modeul1/src/1.6.ts
@@ -0,0 +1,22 @@
+// const hellobro: string = "shakhawat";
+// console.log(hellobro);
+
+// // function use
+
+// function add(number: number, num: number): number {
+//   return number + num;
+// }
+
+// const h = add(2, 4);
+// console.log(h);
+
+// const adds = (num: number, ages: number) => console.log(num + ages);
+// adds(23, 7);
+
+// let bros1: string[] = ["nahid", "shkahkila", "opu"];
+// let bros2: string[] = ["alina", "jonio", "hlkdsfj"];
+
+// bros1.push(...bros2);
+
+const dada: string[] = ["nahid", "shakhawat", "raju"];
+console.log(dada);
--- modeul1/src/index.js
+++ modeul1/src/index.js
@@ -1,5 +1,14 @@
-var course = "shakhawat is next level web developer";
-console.log(course);
-var akash = "tumi ki khawa dawa korco";
-var User = { name: "shakhawat", age: 23 };
-console.log(User);
+// var course = "shakhawat is next level web developer";
+// console.log(course);
+// var akash = "tumi ki khawa dawa korco";
+// var User = { name: "shakhawat", age: 23 };
+// console.log(User);
+
+// const add = (name, age) => {
+//   console.log(name, age);
+// };
+
+// add("shakhawaat", 23);
+
+const old = (name, age) => console.log(name, age);
+old("hi", 23);
